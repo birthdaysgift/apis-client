b8732d42df64cd44bde68e3830a99f4d8cbc240c
Alexey Tsivunin <35929293+birthdaysgift@users.noreply.github.com>
2025-05-07 03:15:01 +0400

    Trying to parse SDL using new SDL schema, but maximum recursion depth exceeded
    That seems correct, since it's possible in graphql to have object with
    fields which refers to the same object
    therefore - here is the recursion
    
    need to find a way to avoid that
    
    when we will build UI elements may be created recursively, but they'll
    be created by user (by hand)
    so max depth recursion will not exist
    
    but our code doesn't try to go as deep as possible, so we have to stop
    schema generation
    
    it seems reasonable to stop schema generation on the object types
    
    so we don't need to calculate object fields deeper, we just need to keep
    a reference to that object

--- py_utils/parse.py
+++ py_utils/parse.py
@@ -1,19 +1,56 @@
+from __future__ import annotations
+
+from enum import Enum
+
+from pydantic import BaseModel
+
 import json
 from pathlib import Path
 
 import requests
 from graphql import get_introspection_query
 
+# from py_utils.sdl_schema import ScalarType, ObjectType, ListType, Field
+
+class Type(BaseModel):
+    scalar_type: ScalarType | None = None
+    list_type: ListType | None = None
+    object_type: ObjectType | None = None
+
+
+class Field(BaseModel):
+    name: str
+    type_: Type
+
+
+class ObjectType(BaseModel):
+    name: str
+    fields: list[Field]
+    required: bool
+
+
+class ScalarType(BaseModel):
+    name: ScalarName
+    required: bool
+
+
+class ListType(BaseModel):
+    type_: Type
+    required: bool
+
+
+class ScalarName(Enum):
+    BOOLEAN = "Boolean"
+    FLOAT = "Float"
+    ID = "ID"
+    INT = "Int"
+    STRING = "String"
+
 
 def main():
     data = get_schema_json("http://localhost:5000/graphql")
-    types = extract_types(data)
-    for main_type in ("Query", "Mutation", "Subscription"):
-        query_type = types.get(main_type)
-        if query_type is None:
-            continue
-        print(f"{main_type}:")
-        parse_fields(query_type["fields"], types, indent=1)
+    parse_schema_json(data).model_dump_json()
+
 
 
 def get_schema_json(source):
@@ -23,42 +60,41 @@ def get_schema_json(source):
     }
     response = requests.post(source, json=introspection_query_body)
     print(response.status_code)
+    (Path(__file__).parent / "api.json").write_text(json.dumps(response.json(), indent=4))
     return response.json()
 
 
-def extract_types(data):
-    return {t["name"]: t for t in data["data"]["__schema"]["types"]}
+def parse_schema_json(data):
+    types = {t["name"]: t for t in data["data"]["__schema"]["types"]}
 
-
-def parse_fields(fields, types, indent=0):
-    for field in fields:
-        description = field.get("description")
-        if description:
-            print(f"{'    '*indent}// {oneline(description)}")
-
-        # raise Exception(
-        #     "TODO: add handling for NON_NULL type modificator, "
-        #     "comment this exception if you want to get current functionality"
-        # )
-
-        type_ = field["type"]
-        type_name = field["type"]["name"]
-        if type_["kind"] == "LIST":
-            type_name = f"[{type_['ofType']['name']}]"
-            print(f"{'    '*indent}{field['name']}: {type_name}")
-            print()
+    for main_type in ("Query", "Mutation", "Introspection"):
+        type_data = types[main_type]
+        if type_data is None:
             continue
 
-        print(f"{'    '*indent}{field['name']}: {type_name}")
-        print()
+        # TODO: now it parses only "Query" but should parse all main types
+        return parse_type(type_data, types, required=False)
 
-        if type_["kind"] == "OBJECT" and type_["name"] in types:
-            indent += 1
-            parse_fields(types[type_['name']]["fields"], types, indent)
-            indent -= 1
 
-def oneline(text):
-    return " ".join(text.split("\n")) if "\n" in text else text
+
+def parse_type(type_data, types, *, required):
+
+    if type_data["kind"] == "SCALAR":
+        return Type(scalar_type=ScalarType(name=type_data["name"], required=required))
+
+    if type_data["kind"] == "NON_NULL":
+        return parse_type(type_data["ofType"], types, required=True)
+
+    if type_data["kind"] == "OBJECT":
+        fields = []
+        for field in types[type_data["name"]]["fields"]:
+            field_type = parse_type(field["type"], types, required=False)
+            fields.append(Field(name=field["name"], type_=field_type))
+        return Type(object_type=ObjectType(name=type_data["name"], fields=fields, required=required))
+
+    if type_data["kind"] == "LIST":
+        list_type = parse_type(type_data["ofType"], types, required=False)
+        return Type(ListType(type_=list_type, required=required))
 
 
 if __name__ == "__main__":
